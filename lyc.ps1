16fade3c3fe7ebae87b48214bba5d52ff7e11b31
Witt Allen <Wittionary@users.noreply.github.com>
2019-03-05 14:40:27 -0600

    Update lyc.ps1

--- lyc.ps1
+++ lyc.ps1
@@ -4,4 +4,32 @@ Function Set-WallPaper($Value)
  rundll32.exe user32.dll, UpdatePerUserSystemParameters
 }
 
+function msgbox {
+    param (
+        [string]$Message,
+        [string]$Title = 'Message box title',   
+        [string]$buttons = 'OKCancel'
+    )
+    # This function displays a message box by calling the .Net Windows.Forms (MessageBox class)
+     
+    # Load the assembly
+    Add-Type -AssemblyName System.Windows.Forms | Out-Null
+     
+    # Define the button types
+    switch ($buttons) {
+       'ok' {$btn = [System.Windows.Forms.MessageBoxButtons]::OK; break}
+       'okcancel' {$btn = [System.Windows.Forms.MessageBoxButtons]::OKCancel; break}
+       'AbortRetryIgnore' {$btn = [System.Windows.Forms.MessageBoxButtons]::AbortRetryIgnore; break}
+       'YesNoCancel' {$btn = [System.Windows.Forms.MessageBoxButtons]::YesNoCancel; break}
+       'YesNo' {$btn = [System.Windows.Forms.MessageBoxButtons]::yesno; break}
+       'RetryCancel'{$btn = [System.Windows.Forms.MessageBoxButtons]::RetryCancel; break}
+       default {$btn = [System.Windows.Forms.MessageBoxButtons]::RetryCancel; break}
+    }
+     
+      # Display the message box
+      $Return=[System.Windows.Forms.MessageBox]::Show($Message,$Title,$btn)
+      $Return
+    }
+    msgbox "test message"
+
 # Set-WallPaper -value "the path of your wallpaper"
