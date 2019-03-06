196c5300a8cfc1f60c6e4ad7299a59ce749368d3
Witt Allen <Wittionary@users.noreply.github.com>
2019-03-06 14:43:01 -0600

    Working state. I think.

--- lyc.ps1
+++ lyc.ps1
@@ -1,35 +1,30 @@
-Function Set-WallPaper($Value)
-{
- Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+$wallpaperPath = "$HOME\Downloads\bg.jpg"
+
+Function Get-WallPaper($wallpaperPath){
+  $imgUrl = @("http://i.imgur.com/NJc5RAH.png",` #mouse
+          "http://patenteux.com/Messy_desktop/messy_wallpaper-1920x1200.jpg",` #messy
+          "https://www.pixelstalk.net/wp-content/uploads/2016/07/1920x1080-Naruto-Wallpapers-HD-620x349.jpg" #naruto
+          )
+  $output = $wallpaperPath
+  (New-Object System.Net.WebClient).DownloadFile((Get-Random -InputObject $imgUrl), $output)
+}
+
+Function Set-WallPaper($wallpaperPath){
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $wallpaperPath
  rundll32.exe user32.dll, UpdatePerUserSystemParameters
 }
 
-function msgbox {
-    param (
-        [string]$Message,
-        [string]$Title = 'Message box title',   
-        [string]$buttons = 'OKCancel'
-    )
-    # This function displays a message box by calling the .Net Windows.Forms (MessageBox class)
-     
-    # Load the assembly
-    Add-Type -AssemblyName System.Windows.Forms | Out-Null
-     
-    # Define the button types
-    switch ($buttons) {
-       'ok' {$btn = [System.Windows.Forms.MessageBoxButtons]::OK; break}
-       'okcancel' {$btn = [System.Windows.Forms.MessageBoxButtons]::OKCancel; break}
-       'AbortRetryIgnore' {$btn = [System.Windows.Forms.MessageBoxButtons]::AbortRetryIgnore; break}
-       'YesNoCancel' {$btn = [System.Windows.Forms.MessageBoxButtons]::YesNoCancel; break}
-       'YesNo' {$btn = [System.Windows.Forms.MessageBoxButtons]::yesno; break}
-       'RetryCancel'{$btn = [System.Windows.Forms.MessageBoxButtons]::RetryCancel; break}
-       default {$btn = [System.Windows.Forms.MessageBoxButtons]::RetryCancel; break}
-    }
-     
-      # Display the message box
-      $Return=[System.Windows.Forms.MessageBox]::Show($Message,$Title,$btn)
-      $Return
-    }
-    msgbox "test message"
-
 # Set-WallPaper -value "the path of your wallpaper"
+Function Set-FlipScreen(){
+  $wshell = New-Object -ComObject wscript.shell;
+  $wshell.SendKeys('^%{DOWN}')
+}
+
+Function Invoke-Mischief(){
+  $functionList = @("Get-WallPaper; Set-WallPaper",`
+                    "Set-FlipScreen")
+ Get-Random -InputObject $functionList
+}
+
+# Execute with powershell (iwr https://raw.githubusercontent.com/Wittionary/Lock-your-computer/master/lyc.ps1).Content
+Invoke-Mischief
