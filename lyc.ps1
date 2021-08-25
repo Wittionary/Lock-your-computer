dac364fe7a39dea735c84ec53bef318dcb54e1db
Witt <Wittionary@users.noreply.github.com>
2021-08-25 10:25:11 -0500

    Updated script

--- lyc.ps1
+++ lyc.ps1
@@ -1,21 +1,30 @@
 $wallpaperPath = "$HOME"
 $wallpaperPath = $wallpaperPath+'\Downloads\bg.jpg'
-Function Get-WallPaper($wallpaperPath){
+
+Function Get-WallPaper(){
+  param (
+    $wallpaperPath = $wallpaperPath
+  )
+
   $imgUrl = @('http://i.imgur.com/NJc5RAH.png',` #mouse
           'http://patenteux.com/Messy_desktop/messy_wallpaper-1920x1200.jpg',` #messy
           'https://www.pixelstalk.net/wp-content/uploads/2016/07/1920x1080-Naruto-Wallpapers-HD-620x349.jpg' #naruto
           )
   $output = $wallpaperPath
   $imgUrl = Get-Random -InputObject $imgUrl
+  Remove-Item $wallpaperPath -Force -ErrorAction SilentlyContinue
   (New-Object System.Net.WebClient).DownloadFile("$imgUrl", "$output")
 }
 
-Function Set-WallPaper($wallpaperPath){
- Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $wallpaperPath
- rundll32.exe user32.dll, UpdatePerUserSystemParameters
+Function Set-WallPaper(){
+  param (
+    $wallpaperPath = $wallpaperPath
+  )
+  
+  Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $wallpaperPath
+  rundll32.exe user32.dll, UpdatePerUserSystemParameters
 }
 
-# Set-WallPaper -value "the path of your wallpaper"
 Function Set-FlipScreen(){
   $wshell = New-Object -ComObject wscript.shell;
   $wshell.SendKeys('^%{DOWN}')
